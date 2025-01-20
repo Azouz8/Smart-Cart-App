e2927fd3da14ab7e431848bbff5cb37d8d1715a9
Ali Azouz <132236484+Azouz8@users.noreply.github.com>
2025-01-20 21:34:45 +0200

    Complete CheckoutCartView

--- lib/features/checkout/presentation/views/checkout_cart_view.dart
+++ lib/features/checkout/presentation/views/checkout_cart_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_cart_app/features/checkout/presentation/views/widgets/checkout_cart_view_body.dart';
 
 class CheckoutCartView extends StatelessWidget {
   const CheckoutCartView({super.key});
@@ -6,16 +7,17 @@ class CheckoutCartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        scrolledUnderElevation: 0,
+        backgroundColor: Colors.transparent,
         title: Text(
           "My Cart",
           style: Theme.of(context).textTheme.headlineMedium,
         ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Checkout View"),
-      ),
+      body: const CheckoutCartViewBody(),
     );
   }
 }
